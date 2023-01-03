84f55572bf3e6971b24de1d4d0e0e5003131393f
Ramm759 <Prozac759>
2023-01-03 16:52:30 +0100

    Ajout repository (@Repository manquant)

--- src/main/java/com/openclassrooms/datalayer/repository/CommentRepositiory.java
+++ src/main/java/com/openclassrooms/datalayer/repository/CommentRepositiory.java
@@ -2,6 +2,8 @@ package com.openclassrooms.datalayer.repository;
 
 import com.openclassrooms.datalayer.model.Comment;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepositiory extends CrudRepository<Comment, Integer> {
 }
